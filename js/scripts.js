20b3b6799cf5c135def0141b3c05fec825cc30c4
Maarja Laineste and Noah Yasskin <maarja@>
2016-06-15 13:54:13 -0700

    add branching to reset the turnTotal to 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 // back-end logic
-var totalScore = 0;
+
+//set global variables
+var playerOneTotalScore = 0;
+var playerTwoTotalScore = 0;
+
 var dieRoll = 0;
 var turnScore = 0;
-function Player(totalScore, turnScore, dieRoll) {
-  this.totalScore = totalScore;
-  this.turnScore = turnScore;
-  this.rollScore = dieRoll;
-  this.activePlayer = true;
-}
-// dice roll generator
+var playerOne = true;
+var playerTwo = false;
+
+//create player objects
+// function Player(totalScore, turnScore, dieRoll) {
+//   this.totalScore = totalScore;
+//   this.turnScore = turnScore;
+//   this.dieRoll = dieRoll;
+//   this.activePlayer = true;
+// }
+// set functions
 
 function randomDieRoller() {
   var randomDieRoll = Math.floor((Math.random() * 6) +1);
@@ -25,8 +33,15 @@ function turnScoreGenerator(dieRoll) {
 }
 
 function totalScoreGenerator(turnScore) {
-  totalScore = totalScore + turnScore;
-  return totalScore;
+  if (playerOne = true) {
+    playerOneTotalScore = playerOneTotalScore + turnScore;
+    return playerOneTotalScore;
+  } else if (playerTwo = true) {
+    playerTwoTotalScore = playerTwoTotalScore + turnScore;
+    return playerTwoTotalScore;
+  } else {
+    alert("totalScoreGenerator is broken");
+  }
 }
 
 // front-end logic
@@ -38,18 +53,33 @@ $(document).ready(function() {
     var dieRoll = randomDieRoller();
     $("#roll-result").text(dieRoll);
     var turnScore = turnScoreGenerator(dieRoll);
-    var playerOne = new Player(totalScore, turnScore, dieRoll)
 
-    $("#p1-turn-total").text(turnScore);
+    // var playerOne = new Player(totalScore, turnScore, dieRoll);
+    // var playerTwo = new Player(totalScore, turnScore, dieRoll);
+    if (playerOne = true) {
+      $("#p1-turn-total").text(turnScore);
+    } else if (playerTwo = false) {
+      $("#p2-turn-total").text(turnScore);
+    } else alert("Broken boolean turn total");
+
+
+  });
 
   $("#hold-button").click(function(event) {
     event.preventDefault();
 
-    var totalScore = totalScoreGenerator(turnScore);
+    if (playerOne = true) {
+      var totalScore = totalScoreGenerator(turnScore);
+      $("#p1-score-total").text(totalScore);
+      playerOne = false;
+    }
+    else if (playerTwo = true) {
+      var totalScore = totalScoreGenerator(turnScore);
+      $("#p2-score-total").text(totalScore);
+      playerTwo = false;
+    }
 
-    $("#p1-score-total").text(totalScore);
-
+    turnScore = 0;
   });
-
-  });
+  $("input#p1-turn-total").val("");
 });
