1713bd00e75c95a14f614ceb357ba8528b424b89
Maarja Laineste and Noah Yasskin <maarja@>
2016-06-16 14:33:57 -0700

    add dice img

--- js/refactored.js
+++ js/refactored.js
@@ -13,7 +13,33 @@ var playerTwo = new Player(false, 0, 0, 0);
 //roll prototype
 Player.prototype.randomDieRoller = function() {
   this.dieRoll = Math.floor((Math.random() * 6) +1);
-  return this.dieRoll;
+  // return this.dieRoll;
+}
+
+Player.prototype.showImg = function() {
+  $("#dice-img").empty();
+  if (this.dieRoll === 1) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.one + ">");
+  } else if (this.dieRoll === 2) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.two + ">");
+  } else if (this.dieRoll === 3) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.three + ">");
+  } else if (this.dieRoll === 4) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.four + ">");
+  } else if (this.dieRoll === 5) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.five + ">");
+  } else if (this.dieRoll === 6) {
+    $("#dice-img").append("<img class='dice-img' src=" + dice.six + ">");
+  }
+}
+
+var dice = {
+  one: "img/1.png",
+  two: "img/2.png",
+  three: "img/3.png",
+  four: "img/4.png",
+  five: "img/5.png",
+  six: "img/6.png"
 }
 
 Player.prototype.turnScoreGenerator = function() {
@@ -25,13 +51,13 @@ Player.prototype.turnScoreGenerator = function() {
   else {
     this.turnScore += this.dieRoll;
   }
-  return this.turnScore;
+  // return this.turnScore;
 }
 // hold prototype
 Player.prototype.totalScoreGenerator = function() {
     this.totalScore += this.turnScore;
     this.turnScore = 0;
-  return this.totalScore;
+  // return this.totalScore;
 }
 // switch prototype
 Player.prototype.playerSwitch = function() {
@@ -68,8 +94,9 @@ Player.prototype.playerStart = function() {
 $(document).ready(function() {
   $("#play-button").click(function(event) {
     event.preventDefault();
-
     playerOne.playerStart();
+
+
 
     $("#p1-dieRoll").text(playerOne.dieRoll);
     $("#p2-dieRoll").text(playerTwo.dieRoll);
@@ -78,7 +105,10 @@ $(document).ready(function() {
     } else {
       $("#p2-header").addClass("header-color");
     }
-    console.log(playerOne.activePlayer);
+    $("#play-button").hide();
+    $("form#pig-dice").each(function() {
+      this.reset();
+    });
   });
 
   $("#roll-button").click(function(event) {
@@ -86,12 +116,14 @@ $(document).ready(function() {
     if (playerOne.activePlayer === true) {
       // $("#p1-header").addClass("header-color");
       playerOne.randomDieRoller();
+      playerOne.showImg();
       playerOne.turnScoreGenerator();
       $("#p1-dieRoll").text(playerOne.dieRoll);
       $("#p1-turn-score").text(playerOne.turnScore);
     } else if (playerTwo.activePlayer === true) {
       // $("#p2-header").addClass("header-color");
       playerTwo.randomDieRoller();
+      playerTwo.showImg();
       playerTwo.turnScoreGenerator();
       $("#p2-dieRoll").text(playerTwo.dieRoll);
       $("#p2-turn-score").text(playerTwo.turnScore);
@@ -109,6 +141,7 @@ $(document).ready(function() {
       if (playerOne.totalScore >= 100) {
         $("#p1-winner").show();
         $("#p2-looser").show();
+        $("#play-button").show();
       }
       playerOne.playerSwitch();
     } else if (playerTwo.activePlayer === true) {
@@ -119,6 +152,7 @@ $(document).ready(function() {
       if (playerTwo.totalScore >= 100) {
         $("#p2-winner").show();
         $("#p1-looser").show();
+        $("#play-button").show();
       }
       playerTwo.playerSwitch();
     } else alert("totalScoreGenerator Broken!");
