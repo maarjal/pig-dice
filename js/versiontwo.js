b76be022b344aa5561a3a7357ee7f592a84e198c
Maarja Laineste and Noah Yasskin <maarja@>
2016-06-16 15:42:26 -0700

    refactored UI out of the business logic, add reset function

--- js/versiontwo.js
+++ js/versiontwo.js
@@ -1,11 +1,36 @@
 //business logic
+function Game(playerOne, playerTwo) {
+  this.playerOne = playerOne;
+  this.playerTwo = playerTwo;
+  this.currentPlayer = this.playerStart();
+}
+
+var newGame = new Game(playerOne, playerTwo);
+
+
+Game.prototype.playerStart = function() {
+  this.playerOne.dieRoll = this.playerOne.randomDieRoller();
+  playerthis.randomDieRoller();
+
+  if (playerOne.dieRoll > playerTwo.dieRoll) {
+    this.playerOne.activePlayer = true;
+    this.playerTwo.activePlayer = false;
+  }
+  else if (playerOne.dieRoll < playerTwo.dieRoll) {
+      playerOne.activePlayer = false;
+      playerTwo.activePlayer = true;
+  } else {
+    playerOne.playerStart();
+  }
+}
+
+
 function Player(activePlayer) {
   this.activePlayer = activePlayer;
   this.dieRoll = 0;
   this.turnScore = 0;
   this.totalScore = 0;
 }
-
 //initialize new player
 var playerOne = new Player(true);
 var playerTwo = new Player(false);
@@ -13,6 +38,7 @@ var playerTwo = new Player(false);
 //roll prototype
 Player.prototype.randomDieRoller = function() {
   this.dieRoll = Math.floor((Math.random() * 6) +1);
+  // return this.dieRoll;
 }
 
 Player.prototype.turnScoreGenerator = function() {
@@ -24,11 +50,13 @@ Player.prototype.turnScoreGenerator = function() {
   else {
     this.turnScore += this.dieRoll;
   }
+  // return this.turnScore;
 }
 // hold prototype
 Player.prototype.totalScoreGenerator = function() {
     this.totalScore += this.turnScore;
     this.turnScore = 0;
+  // return this.totalScore;
 }
 // switch prototype
 Player.prototype.playerSwitch = function() {
@@ -45,38 +73,11 @@ Player.prototype.playerSwitch = function() {
   }
 }
 
-Player.prototype.playerStart = function() {
-  playerOne.randomDieRoller();
-  playerTwo.randomDieRoller();
 
-  if (playerOne.dieRoll > playerTwo.dieRoll) {
-    playerOne.activePlayer = true;
-    playerTwo.activePlayer = false;
-  }
-  else if (playerOne.dieRoll < playerTwo.dieRoll) {
-      playerOne.activePlayer = false;
-      playerTwo.activePlayer = true;
-  } else {
-    playerOne.playerStart();
-  }
-}
 
-function resetFields() {
-  $("span#p1-dieRoll").empty();
-  $("span#p2-dieRoll").empty();
-  $("span#p1-turn-score").empty();
-  $("span#p2-turn-score").empty();
-  $("span#p1-total-score").empty();
-  $("span#p2-total-score").empty();
-  $("h4#p1-winner").hide();
-  $("h4#p2-winner").hide();
-  $("h4#p1-looser").hide();
-  $("h4#p2-looser").hide();
-}
 
-// ui functions
+//UI FUNCTION
 var showImg = function(dieRoll) {
-  $("#dice-img").empty();
   var dice = {
     one: "img/1.png",
     two: "img/2.png",
@@ -85,6 +86,8 @@ var showImg = function(dieRoll) {
     five: "img/5.png",
     six: "img/6.png"
   }
+
+  $("#dice-img").empty();
   if (dieRoll === 1) {
     $("#dice-img").append("<img class='dice-img' src=" + dice.one + ">");
   } else if (dieRoll === 2) {
@@ -100,15 +103,9 @@ var showImg = function(dieRoll) {
   }
 }
 
-var showWinner1 = function() {
+var showWinner = function() {
   $("#p1-winner").show();
   $("#p2-looser").show();
-  $("#play-button").show();
-}
-
-var showWinner2 = function() {
-  $("#p2-winner").show();
-  $("#p1-looser").show();
   $("#play-button").show();
 }
 
@@ -116,8 +113,8 @@ var showWinner2 = function() {
 $(document).ready(function() {
   $("#play-button").click(function(event) {
     event.preventDefault();
-    resetFields();
     playerOne.playerStart();
+
     $("#p1-dieRoll").text(playerOne.dieRoll);
     $("#p2-dieRoll").text(playerTwo.dieRoll);
     if (playerOne.activePlayer === true) {
@@ -131,12 +128,18 @@ $(document).ready(function() {
   $("#roll-button").click(function(event) {
     event.preventDefault();
     if (playerOne.activePlayer === true) {
+      // $("#p1-header").addClass("header-color");
       playerOne.randomDieRoller();
       showImg(playerOne.dieRoll);
+      //playerOne.showImg();
       playerOne.turnScoreGenerator();
+      if (playerOne.dieRoll === 1) {
+        //change header with jqery
+      }
       $("#p1-dieRoll").text(playerOne.dieRoll);
       $("#p1-turn-score").text(playerOne.turnScore);
     } else if (playerTwo.activePlayer === true) {
+      // $("#p2-header").addClass("header-color");
       playerTwo.randomDieRoller();
       showImg(playerTwo.dieRoll);
       playerTwo.turnScoreGenerator();
@@ -147,13 +150,14 @@ $(document).ready(function() {
 
   $("#hold-button").click(function(event) {
     event.preventDefault();
+
     if (playerOne.activePlayer === true) {
       $(".header-color").removeClass();
       $("#p2-header").addClass("header-color");
       playerOne.totalScoreGenerator();
       $("#p1-total-score").text(playerOne.totalScore);
       if (playerOne.totalScore >= 100) {
-        showWinner1();
+        showWinner();
       }
       playerOne.playerSwitch();
     } else if (playerTwo.activePlayer === true) {
@@ -162,7 +166,7 @@ $(document).ready(function() {
       playerTwo.totalScoreGenerator();
       $("#p2-total-score").text(playerTwo.totalScore);
       if (playerTwo.totalScore >= 100) {
-        showWinner2();
+        showWinner();
       }
       playerTwo.playerSwitch();
     }
